cb376bf6ba608ac4e2835e4da5ae8efa45fb38ef
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-25 07:25:06 +1000

    commit tree a72d36064aa466eac5a30e475ef2c064b1689dbc

---
+++ lib/insert.sql
@@ -0,0 +1,44 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES ("Mr. Chocolate", 10, "M", "brown", "happy", 1);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES ("Rowdy", 8, "M", "black", "nervous", 1);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES ("Tabitha", 10, "F", "grey", "happy", 0);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES ("Sergeant Brown", 7, "M", "black", "rude", 1);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES ("Melissa", 3, "F", "white", "happy", 1);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES ("Grinch", 12, "M", "brown", "rude", 0);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES ("Wendy", 9, "F", "brown", "nervous", 1);
+
+INSERT INTO bears(age,sex,color,temperament,alive)
+VALUES (7, "M", "black", "nervous", 0);
+
+-- SELECT name, age
+-- FROM bears
+-- WHERE sex = "F"
+
+-- SELECT name FROM bears
+-- ORDER BY name
+
+-- SELECT name, age FROM bears
+-- WHERE alive = 1
+-- ORDER BY age
+
+-- SELECT name, age FROM bears
+-- ORDER BY age DESC
+-- LIMIT 1
+
+-- SELECT color, COUNT(color) FROM bears
+-- GROUP BY color
+-- ORDER BY COUNT(color) DESC
+-- LIMIT 1;
+
